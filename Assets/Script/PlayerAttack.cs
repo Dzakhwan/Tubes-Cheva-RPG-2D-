6a1497bac97b2e4244d048f7cfedcf6dae960e77
Dzakhwan <Dzakhwantastic@gmail.com>
2025-08-02 23:32:23 +0700

    fix slash

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject slashEffect;                   // Prefab slash effect
@@ -6,20 +8,23 @@ public class PlayerAttack : MonoBehaviour
     private Vector2 lastMoveDir = Vector2.right;
     public LayerMask enemyLayer;
 
+    public float slashDuration = 0.5f;               // Waktu efek slash bertahan
+    public float slashDelay = 0.1f;                  // Jeda sebelum slash muncul
 
-    public float slashDuration = 0.5f;
-
-    public Transform weaponParent;                   // Parent senjata
-    public GameObject weaponObject;                  // Senjata (hand_sword_0)
-    public Animator anim;                            // Animator player
+    public Transform weaponParent;
+    public GameObject weaponObject;
+    public Animator anim;
     public float attackDuration = 0.3f;
 
     private bool isAttacking = false;
     private float attackTimer = 0f;
 
+    private int comboStep = 0;
+    private float comboResetTime = 1f;
+    private float comboTimer = 0f;
+
     void Update()
     {
-
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (moveInput != Vector2.zero)
         {
@@ -28,7 +33,13 @@ public class PlayerAttack : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) && !isAttacking)
         {
-            StartAttack();
+            comboStep++;
+            comboTimer = comboResetTime;
+
+            if (comboStep > 2)
+                comboStep = 1;
+
+            StartAttack(comboStep);
         }
 
         if (isAttacking)
@@ -39,96 +50,89 @@ public class PlayerAttack : MonoBehaviour
                 EndAttack();
             }
         }
+
+        if (comboStep > 0)
+        {
+            comboTimer -= Time.deltaTime;
+            if (comboTimer <= 0f)
+            {
+                comboStep = 0;
+            }
+        }
     }
 
-    void StartAttack()
+    void StartAttack(int comboIndex)
     {
         isAttacking = true;
         attackTimer = attackDuration;
 
         if (anim != null)
-            anim.SetTrigger("Attack");
+            anim.SetTrigger("Attack" + comboIndex);
 
         Transform selectedAttackPoint = attackPoint;
-
         if (Mathf.Abs(lastMoveDir.y) > Mathf.Abs(lastMoveDir.x))
         {
-            if (lastMoveDir.y > 0)
-                selectedAttackPoint = attackPointUp;
-            else
-                selectedAttackPoint = attackPointDown;
+            selectedAttackPoint = lastMoveDir.y > 0 ? attackPointUp : attackPointDown;
         }
 
-        GameObject slash = Instantiate(slashEffect, selectedAttackPoint.position, Quaternion.identity);
+        // Kirim posisi dan arah saat itu ke coroutine
+        StartCoroutine(DelayedSlash(selectedAttackPoint.position, lastMoveDir));
+    }
+
+    IEnumerator DelayedSlash(Vector3 slashPosition, Vector2 moveDir)
+    {
+        yield return new WaitForSeconds(slashDelay);
+
+        GameObject slash = Instantiate(slashEffect, slashPosition, Quaternion.identity);
         Destroy(slash, slashDuration);
 
-        // Rotasi
-        if (lastMoveDir.x > 0)
+        // Rotasi efek berdasarkan arah saat serang
+        if (moveDir.x > 0)
             slash.transform.rotation = Quaternion.Euler(0, 0, 0);
-        else if (lastMoveDir.x < 0)
+        else if (moveDir.x < 0)
             slash.transform.rotation = Quaternion.Euler(0, 180, 0);
-        else if (lastMoveDir.y > 0)
+        else if (moveDir.y > 0)
             slash.transform.rotation = Quaternion.Euler(0, 0, 90);
-        else if (lastMoveDir.y < 0)
+        else if (moveDir.y < 0)
             slash.transform.rotation = Quaternion.Euler(0, 0, -90);
 
-        // Serang musuh di area
-        Debug.Log("Attacking at position: " + selectedAttackPoint.position + " with range: " + StatsManager.Instance.attackRange);
+        // Deteksi musuh di posisi saat itu
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            slashPosition,
+            StatsManager.Instance.attackRange,
+            enemyLayer
+        );
 
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(selectedAttackPoint.position, StatsManager.Instance.attackRange, enemyLayer);
-        Debug.Log("Found " + hitEnemies.Length + " enemies in range");
         if (hitEnemies.Length > 0)
         {
             hitEnemies[0].GetComponent<EnemyHealth>().ChangeHealth(-StatsManager.Instance.damageAmount);
-            hitEnemies[0].GetComponent<EnemyKnockback>().knockback(transform, StatsManager.Instance.knockbackForce, StatsManager.Instance.stunTime, StatsManager.Instance.knockbackTime);
+            hitEnemies[0].GetComponent<EnemyKnockback>().knockback(
+                transform,
+                StatsManager.Instance.knockbackForce,
+                StatsManager.Instance.stunTime,
+                StatsManager.Instance.knockbackTime
+            );
         }
-
-        // foreach (Collider2D enemy in hitEnemies)
-        // {
-        //     Debug.Log("Hit enemy: " + enemy.name);
-
-        //     EnemyHealth health = enemy.GetComponent<EnemyHealth>();
-
-        //     if (health != null)
-        //     {
-        //         Debug.Log("Dealing damage to: ");
-        //         health.ChangeHealth(-damageAmount);
-        //     }
-        //     EnemyKnockback knockback = enemy.GetComponent<EnemyKnockback>();
-        //     if (knockback != null)
-        //     {
-        //         knockback.knockback(transform, knockbackForce);
-        //     }
-
-
-
-        // }
     }
 
     void EndAttack()
     {
         isAttacking = false;
     }
+
     void OnDrawGizmosSelected()
-{
-    if (StatsManager.Instance == null) return;
-
-    Gizmos.color = Color.red;
-
-    // Gambar hanya arah serang terakhir
-    Transform selectedAttackPoint = attackPoint;
-
-    if (Mathf.Abs(lastMoveDir.y) > Mathf.Abs(lastMoveDir.x))
     {
-        if (lastMoveDir.y > 0)
-            selectedAttackPoint = attackPointUp;
-        else
-            selectedAttackPoint = attackPointDown;
+        if (StatsManager.Instance == null) return;
+
+        Gizmos.color = Color.red;
+
+        Transform selectedAttackPoint = attackPoint;
+        if (Mathf.Abs(lastMoveDir.y) > Mathf.Abs(lastMoveDir.x))
+        {
+            selectedAttackPoint = lastMoveDir.y > 0 ? attackPointUp : attackPointDown;
+        }
+
+        if (selectedAttackPoint != null)
+            Gizmos.DrawWireSphere(selectedAttackPoint.position, StatsManager.Instance.attackRange);
     }
-
-    if (selectedAttackPoint != null)
-        Gizmos.DrawWireSphere(selectedAttackPoint.position, StatsManager.Instance.attackRange);
 }
-
-
-} 
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (isKnockedBack == false)
         {
