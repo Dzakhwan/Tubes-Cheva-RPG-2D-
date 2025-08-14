17da7ce1f96f828da1cef0031a432a871e330d63
REVIRA9 <96121841+REVIRA9@users.noreply.github.com>
2025-08-14 13:25:35 +0700

    Namabah Teleport ke rumah

---
+++ Assets/Sprite/MAPS/Map-Transition/MapTransition.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+using Unity.Cinemachine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry;
+    [SerializeField] Direction direction;
+    [SerializeField] Transform teleportTargetPosition;
+    [SerializeField] GameObject player;
+    CinemachineConfiner2D confiner;
+
+    enum Direction { Up, Down, Left, Right, Teleport }
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    void UpdatePlayerPosition(GameObject playerObj)
+    {
+        if (direction == Direction.Teleport)
+        {
+            playerObj.transform.position = teleportTargetPosition.position;
+
+            return;
+        }
+        Vector3 additivePos = playerObj.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                additivePos.y += 2;
+                break;
+            case Direction.Down:
+                additivePos.y += -2;
+                break;
+            case Direction.Left:
+                additivePos.x += -2;
+                break;
+            case Direction.Right:
+                additivePos.x += 2;
+                break;
+        }
+        playerObj.transform.position = additivePos;
+    }
+}
